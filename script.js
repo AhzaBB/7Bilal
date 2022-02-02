dc93b10360de3dbc11cc284afc17abe2b7d615e6
AhzaBB <98722934+AhzaBB@users.noreply.github.com>
2022-02-02 20:30:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+const image = document.getElementById('img');
+const guruImg = document.getElementById('guru');
+const namaGuru = document.getElementById('namaGuru');
+const muridImg = document.getElementById('murid');
+const namaMurid = document.getElementById('namaMurid');
+const jabatanMurid = document.getElementById('jabatanMurid');
+
+let imgArr = [
+    '<img src="img/gambar1.png" alt="gambar1">',
+    '<img src="img/gambar2.png" alt="gambar2">'
+]
+let imgI = 0;
+
+let guruImgArr = [
+    '<img src="img/guru1.png" alt="Pak Adi">',
+    '<img src="img/guru2.png" alt="Miss Zuma">'
+]
+let namaGuruArr = [
+    'Pak Adi',
+    'Miss Zuma'
+]
+let guruI = 0;
+
+let namamuridArr = [
+    'Andien',
+    'Aji',
+    'Alma',
+    'Alvarro',
+    'Amira',
+    'Athifah',
+    'Zhafira',
+    'Callysta',
+    'Ahza',
+    'Kanya',
+    'Kynan',
+    'Ammar',
+    'Azzam',
+    'Angga',
+    'Daffa',
+    'Nayla',
+    'Aya',
+    'Salsa',
+    'Saskia'
+]
+let jabatanMuridArr = [
+    'Bendahara<hr>Osis',
+    '-',
+    'Osis',
+    '-',
+    '-',
+    '-',
+    'Osis',
+    'Sekretaris<hr>Osis',
+    '-',
+    '-',
+    'Ketua Kelas<hr>Osis',
+    '-',
+    '-',
+    'Wakil Ketua Kelas',
+    '-',
+    'Wakil Ketua Osis',
+    '-',
+    '-',
+    'Osis'
+]
+let muridI = 0;
+
+
+function imgIP() {
+    if (imgI < imgArr.length - 1) {
+        imgI++;
+    }
+}
+function imgIM() {
+    if (imgI > 0) {
+        imgI--;
+    }
+}
+
+function guruImgIP() {
+    if (guruI < guruImgArr.length - 1) {
+        guruI++;
+    }
+}
+function guruImgIM() {
+    if (guruI > 0) {
+        guruI--;
+    }
+}
+
+
+function muridImgIP() {
+    if (muridI < namamuridArr.length - 1) {
+        muridI++;
+    }
+}
+function muridImgIM() {
+    if (muridI > 0) {
+        muridI--;
+    }
+    console.log(`<img src"img/murid${muridI+1}.png" alt="${namamuridArr[muridI]}">`)
+}
+
+function update() {
+    image.innerHTML = imgArr[imgI];
+    guruImg.innerHTML = guruImgArr[guruI];
+    namaGuru.innerHTML = namaGuruArr[guruI];
+    muridImg.innerHTML = `<img src="img/murid${muridI+1}.png" alt="${namamuridArr[muridI]}">`
+    jabatanMurid.innerHTML = jabatanMuridArr[muridI]
+    namaMurid.innerHTML = namamuridArr[muridI];
+    setTimeout(update);
+}
+
+update()
